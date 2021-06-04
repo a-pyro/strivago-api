f998c00aeba998ac71b48df52c4a4a648fd19df7
Hafiz Ali <74325320+alihfph@users.noreply.github.com>
2021-06-04 15:12:54 +0200

    this.commit

--- src/routes/accomodations.js
+++ src/routes/accomodations.js
@@ -6,8 +6,8 @@ const router = Router();
 
 router.get('/', async (req, res, next) => {
   try {
-    // const accomodations = await AccomodationModel.find({});
-    // res.status(200).send(accomodations);
+    const accomodations = await AccomodationModel.find({});
+    res.status(200).send(accomodations);
   } catch (error) {
     next(error);
   }
@@ -24,6 +24,14 @@ router.post('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
+
+    const id = req.params.id
+    const newAccomodation = await AccomodationModel.findById(id)
+    if (newAccomodation){
+      res.status(200).send(newAccomodation)
+    } else {
+      next(new ErrorResponse("ID was not found", 404))
+    }
   } catch (error) {
     next(error);
   }
@@ -31,6 +39,16 @@ router.get('/:id', async (req, res, next) => {
 
 router.put('/:id', async (req, res, next) => {
   try {
+    const id = req.params.id
+    const newAccomodation = await AccomodationModel.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+      new: true,
+    })
+    if (newAccomodation){
+      res.status(200).send(newAccomodation)
+    } else {
+      next(new ErrorResponse("ID was not found", 404))
+    }
   } catch (error) {
     next(error);
   }
@@ -38,6 +56,9 @@ router.put('/:id', async (req, res, next) => {
 
 router.delete('/:id', async (req, res, next) => {
   try {
+    const id = req.params.id
+    const newAccomodation = await AccomodationModel.findByIdAndDelete(id)
+    res.status(204).send()
   } catch (error) {
     next(error);
   }
