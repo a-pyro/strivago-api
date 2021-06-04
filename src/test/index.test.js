46c0a3255f350225a431a5bce5882923ce539c61
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-06-04 12:45:13 +0200

    boilerplater

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -4,3 +4,24 @@ import { app } from '../server.js';
 dotenv.config();
 
 const request = supertest(app);
+
+beforeAll((done) => {
+  console.log(process.env.MONGO_URL);
+  mongoose
+    .connect(process.env.ATLAS_URL + 'test', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Successfully connected to Atlas in test.');
+      done();
+    });
+});
+
+afterAll((done) => {
+  mongoose.connection.dropDatabase(() => {
+    mongoose.connection.close(() => done());
+  });
+});
